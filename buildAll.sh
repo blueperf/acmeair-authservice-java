3d54da75ebb2967901b02b5a6b4ff16da28b45cd
ruizhao <rui.zhao1@ibm.com>
2023-08-15 14:38:45 -0500

    update wildfly

---
+++ buildAll.sh
@@ -0,0 +1,5 @@
+docker build -t ol-auth .
+docker build -t wf-auth -f Dockerfile-wf .
+docker build -t qu-auth -f Dockerfile-qu .
+docker build -t qn-auth -f Dockerfile-qn .
+docker build -t pm-auth -f Dockerfile-pm .
